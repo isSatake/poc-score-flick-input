504b0fefa77eea9e301969c59ee06e3bcae0004a
isSatake <stkayy@gmail.com>
2020-11-22 11:19:50 +0900

    draw staff lines

--- index.js
+++ index.js
@@ -3,7 +3,27 @@ const initCanvas = () => {
     const canvas = document.getElementById("canvas");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    return canvas;
 };
-window.onload = (e) => {
-    initCanvas();
+const initStaffLines = (ctx) => {
+    const HEIGHT_HEAD = 40;
+    const LEFT_STAFF = 20;
+    const TOP_STAFF = 300;
+    const WIDTH_LINE = 1;
+    const WIDTH_STAFF = 800;
+    for (let i = 0; i < 5; i++) {
+        const y = TOP_STAFF + HEIGHT_HEAD * i;
+        ctx.strokeStyle = "#000";
+        ctx.beginPath();
+        ctx.moveTo(LEFT_STAFF, y);
+        ctx.lineTo(LEFT_STAFF + WIDTH_STAFF, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+};
+window.onload = () => {
+    const canvasCtx = initCanvas().getContext("2d");
+    if (canvasCtx == null)
+        return;
+    initStaffLines(canvasCtx);
 };
