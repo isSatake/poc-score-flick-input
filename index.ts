15054961f82398e73816e0ef1eb53ce094acabce
isSatake <stkayy@gmail.com>
2020-11-23 17:58:54 +0900

    draw note

--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
 const HEIGHT_STAFF_BRAVURA = 1000
-const GCLEF_PATH = "M364,-252c-245,0 -364,165 -364,339c0,202 153,345 297,464c12,10 11,12 9,24c-7,41 -14,106 -14,164c0,104 24,229 98,311c20,22 51,48 65,48c11,0 37,-28 52,-50c41,-60 65,-146 65,-233c0,-153 -82,-280 -190,-381c-6,-6 -8,-7 -6,-19l25,-145c3,-18 3,-18 29,-18c147,0 241,-113 241,-241c0,-113 -67,-198 -168,-238c-14,-6 -15,-5 -13,-17c11,-62 29,-157 29,-214c0,-170 -130,-200 -197,-200c-151,0 -190,98 -190,163c0,62 40,115 107,115c61,0 96,-47 96,-102c0,-58 -36,-85 -67,-94c-23,-7 -32,-10 -32,-17c0,-13 26,-29 80,-29c59,0 159,18 159,166c0,47 -15,134 -27,201c-2,12 -4,11 -15,9c-20,-4 -46,-6 -69,-6zM80,20c0,-139 113,-236 288,-236c20,0 40,2 56,5c15,3 16,3 14,14l-50,298c-2,11 -4,12 -20,8c-61,-17 -100,-60 -100,-117c0,-46 30,-89 72,-107c7,-3 15,-6 15,-13c0,-6 -4,-11 -12,-11c-7,0 -19,3 -27,6c-68,23 -115,87 -115,177c0,85 57,164 145,194c18,6 18,5 15,24l-21,128c-2,11 -4,12 -14,4c-47,-38 -93,-75 -153,-142c-83,-94 -93,-173 -93,-232zM470,943c-61,0 -133,-96 -133,-252c0,-32 2,-66 6,-92c2,-13 6,-14 13,-8c79,69 174,159 174,270c0,55 -27,82 -60,82zM441,117c-12,1 -13,-2 -11,-14l49,-285c2,-12 4,-12 16,-6c56,28 94,79 94,142c0,88 -67,156 -148,163z"
+const PATH2D_GCLEF = new Path2D(
+  "M364,-252c-245,0 -364,165 -364,339c0,202 153,345 297,464c12,10 11,12 9,24c-7,41 -14,106 -14,164c0,104 24,229 98,311c20,22 51,48 65,48c11,0 37,-28 52,-50c41,-60 65,-146 65,-233c0,-153 -82,-280 -190,-381c-6,-6 -8,-7 -6,-19l25,-145c3,-18 3,-18 29,-18c147,0 241,-113 241,-241c0,-113 -67,-198 -168,-238c-14,-6 -15,-5 -13,-17c11,-62 29,-157 29,-214c0,-170 -130,-200 -197,-200c-151,0 -190,98 -190,163c0,62 40,115 107,115c61,0 96,-47 96,-102c0,-58 -36,-85 -67,-94c-23,-7 -32,-10 -32,-17c0,-13 26,-29 80,-29c59,0 159,18 159,166c0,47 -15,134 -27,201c-2,12 -4,11 -15,9c-20,-4 -46,-6 -69,-6zM80,20c0,-139 113,-236 288,-236c20,0 40,2 56,5c15,3 16,3 14,14l-50,298c-2,11 -4,12 -20,8c-61,-17 -100,-60 -100,-117c0,-46 30,-89 72,-107c7,-3 15,-6 15,-13c0,-6 -4,-11 -12,-11c-7,0 -19,3 -27,6c-68,23 -115,87 -115,177c0,85 57,164 145,194c18,6 18,5 15,24l-21,128c-2,11 -4,12 -14,4c-47,-38 -93,-75 -153,-142c-83,-94 -93,-173 -93,-232zM470,943c-61,0 -133,-96 -133,-252c0,-32 2,-66 6,-92c2,-13 6,-14 13,-8c79,69 174,159 174,270c0,55 -27,82 -60,82zM441,117c-12,1 -13,-2 -11,-14l49,-285c2,-12 4,-12 16,-6c56,28 94,79 94,142c0,88 -67,156 -148,163z"
+)
+const PATH2D_NOTE_HEAD = new Path2D(
+  "M0 -42c0 86 88 167 198 167c57 0 97 -32 97 -83c0 -85 -109 -167 -198 -167c-54 0 -97 31 -97 83z"
+)
 
 const initCanvas = (): HTMLCanvasElement => {
   const canvas = document.getElementById("canvas") as HTMLCanvasElement
@@ -9,12 +14,12 @@ const initCanvas = (): HTMLCanvasElement => {
 }
 
 const drawGClef = (ctx: CanvasRenderingContext2D, left: number, topOfStaff: number, scale: number) => {
-  const path2d = new Path2D(GCLEF_PATH)
   const y = topOfStaff + (HEIGHT_STAFF_BRAVURA / 4 * scale) * 3
   ctx.rotate((Math.PI / 180) * 180) // もとのパスは回転している
   ctx.translate(-left, -y) // 原点を五線上のGの高さに移動(回転しているため負の値)
   ctx.scale(-scale, scale) // もとのパスは五線の高さを1000としているのでスケールする
-  ctx.fill(path2d)
+  ctx.fill(PATH2D_GCLEF)
+  ctx.resetTransform()
 }
 
 const drawStaff = (ctx: CanvasRenderingContext2D, left: number, top: number, width: number, scale: number) => {
@@ -31,12 +36,25 @@ const drawStaff = (ctx: CanvasRenderingContext2D, left: number, top: number, wid
   }
 }
 
+const drawNote = (ctx: CanvasRenderingContext2D, topOfStaff: number, left: number, index: number, scale: number) => {
+  // y原点は符頭の中心(音程を示す高さ)
+  const halfOfNoteHeadHeight = HEIGHT_STAFF_BRAVURA * scale / 8
+  const e2y = topOfStaff + 1750 * scale // scale=1, index=0(E2)のときY=1750
+  const top = e2y - (index * halfOfNoteHeadHeight) + halfOfNoteHeadHeight
+  ctx.rotate((Math.PI / 180) * 180)
+  ctx.translate(-left, -top)
+  ctx.scale(-scale, scale)
+  ctx.fill(PATH2D_NOTE_HEAD)
+  ctx.resetTransform()
+}
+
 window.onload = () => {
   const canvasCtx = initCanvas().getContext("2d")
   if (canvasCtx == null) return
   const scale = 0.2
-  const left = 20
-  const top = 500
-  drawStaff(canvasCtx, left, top, window.innerWidth - left * 2, scale)
-  drawGClef(canvasCtx, left + 30, top, scale)
+  const marginHorizontal = 20
+  const topOfStaff = 500
+  drawStaff(canvasCtx, marginHorizontal, topOfStaff, window.innerWidth - marginHorizontal * 2, scale)
+  drawGClef(canvasCtx, marginHorizontal + 30, topOfStaff, scale)
+  drawNote(canvasCtx, topOfStaff, 250, 21, scale)
 }
