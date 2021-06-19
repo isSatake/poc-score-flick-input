34d9ea8ec1d7f422a8934efdc62fe52a4dbbf790
satake <stkayy@gmail.com>
2021-06-19 18:14:09 +0900

    wip: accidental

--- index.ts
+++ index.ts
@@ -17,19 +17,19 @@ import {
   EXTENSION_LEDGER_LINE,
   FlagDown,
   FlagUp,
-  HEIGHT_STAFF_BRAVURA,
+  bStaffHeight,
   Path,
-  PATH2D_GCLEF,
-  PATH2D_NOTE_HEAD,
-  PATH2D_NOTE_HEAD_HALF,
-  PATH2D_NOTE_HEAD_WHOLE,
+  bClefG,
+  bNoteHead,
+  bNoteHeadHalf,
+  bNoteHeadWhole,
   RestPath,
   UNIT,
   WIDTH_NOTE_HEAD_BLACK,
   WIDTH_NOTE_HEAD_WHOLE,
-  WIDTH_STAFF_LEDGER_LINE,
-  WIDTH_STAFF_LINE,
-  WIDTH_STEM,
+  bLedgerLineWidth,
+  bStaffLineWidth,
+  bStemWidth,
 } from "./bravura";
 
 type Duration = 1 | 2 | 4 | 8 | 16 | 32;
@@ -81,14 +81,14 @@ const accidentalPathMap = new Map<Accidental, Path>([
   ["flat", bAccidentalFlat],
 ]);
 
-const noteHeadByDuration = (duration: Duration): Path2D => {
+const noteHeadByDuration = (duration: Duration): Path => {
   switch (duration) {
     case 1:
-      return PATH2D_NOTE_HEAD_WHOLE;
+      return bNoteHeadWhole;
     case 2:
-      return PATH2D_NOTE_HEAD_HALF;
+      return bNoteHeadHalf;
     default:
-      return PATH2D_NOTE_HEAD;
+      return bNoteHead;
   }
 };
 
@@ -111,13 +111,13 @@ const drawBravuraPath = (
   left: number,
   top: number,
   scale: number,
-  path: Path2D
+  path: Path
 ) => {
   ctx.save();
   ctx.rotate((Math.PI / 180) * 180); // もとのパスは回転している
   ctx.translate(-left, -top); // 回転しているため負の値
   ctx.scale(-scale, scale); // もとのパスは五線の高さを1000としているのでスケールする
-  ctx.fill(path);
+  ctx.fill(path.path2d);
   ctx.restore();
 };
 
@@ -128,7 +128,7 @@ const drawGClef = (
   scale: number
 ) => {
   const y = topOfStaff + UNIT * scale * 3; // 原点を五線上のGの高さに移動
-  drawBravuraPath(ctx, left, y, scale, PATH2D_GCLEF);
+  drawBravuraPath(ctx, left, y, scale, bClefG);
 };
 
 const drawStaff = (
@@ -143,7 +143,7 @@ const drawStaff = (
     const y = top + heightHead * i;
     ctx.save();
     ctx.strokeStyle = "#000";
-    ctx.lineWidth = WIDTH_STAFF_LINE * scale;
+    ctx.lineWidth = bStaffLineWidth * scale;
     ctx.beginPath();
     ctx.moveTo(left, y);
     ctx.lineTo(left + width, y);
@@ -157,23 +157,33 @@ const pitchToY = (topOfStaff: number, pitch: Pitch, scale: number): number => {
   // ギターの音域に合わせ最低音をE2(=index:0)としている
   // middleC(C4)=0とする
   // y原点は符頭の中心(音程を示す高さ)
-  const halfOfNoteHeadHeight = (HEIGHT_STAFF_BRAVURA * scale) / 8;
+  const halfOfNoteHeadHeight = (bStaffHeight * scale) / 8;
   const c4y = topOfStaff + UNIT * 4.5 * scale + halfOfNoteHeadHeight;
   //   const e2y = topOfStaff + 1750 * scale + halfOfNoteHeadHeight; // scale=1, index=0(E2)のときY=1750
   return c4y - pitch * halfOfNoteHeadHeight;
 };
 
-const drawNoteHead = (params: DrawNoteParams) => {
+type DrawnSection = {
+  start: number;
+  end: number;
+};
+
+const calcSection = (
+  start: number,
+  scale: number,
+  path: Path
+): DrawnSection => {
+  const width = (path.bbox.ne.x - path.bbox.sw.x) * UNIT * scale;
+  return { start, end: start + width };
+};
+
+const drawNoteHead = (params: DrawNoteParams): DrawnSection => {
   const { ctx, leftOfNoteHead, topOfStaff, note, scale } = params;
   const { pitch, duration } = note;
   const top = pitchToY(topOfStaff, pitch, scale);
-  drawBravuraPath(
-    ctx,
-    leftOfNoteHead,
-    top,
-    scale,
-    noteHeadByDuration(duration)
-  );
+  const path = noteHeadByDuration(duration);
+  drawBravuraPath(ctx, leftOfNoteHead, top, scale, path);
+  return calcSection(leftOfNoteHead, scale, path);
 };
 
 const drawLedgerLine = (
@@ -182,7 +192,7 @@ const drawLedgerLine = (
   leftOfNoteHead: number,
   note: Note,
   scale: number
-) => {
+): DrawnSection => {
   // note headからはみ出る長さ
   const extension =
     noteHeadWidth(note.duration) * EXTENSION_LEDGER_LINE * scale;
@@ -190,22 +200,24 @@ const drawLedgerLine = (
   const end = start + noteHeadWidth(note.duration) * scale + extension * 2;
   ctx.save();
   ctx.strokeStyle = "#000";
-  ctx.lineWidth = WIDTH_STAFF_LEDGER_LINE * scale;
+  ctx.lineWidth = bLedgerLineWidth * scale;
   ctx.beginPath();
   ctx.moveTo(start, top);
   ctx.lineTo(end, top);
   ctx.closePath();
   ctx.stroke();
   ctx.restore();
+  return { start, end };
 };
 
-const drawLedgerLines = (params: DrawNoteParams) => {
+const drawLedgerLines = (params: DrawNoteParams): DrawnSection | undefined => {
   const { ctx, note, scale, leftOfNoteHead, topOfStaff } = params;
   const { pitch } = note;
+  let section: DrawnSection | undefined;
   if (pitch <= 0) {
     // 0=C4
     for (let i = 0; i >= pitch; i -= 2) {
-      drawLedgerLine(
+      section = drawLedgerLine(
         ctx,
         pitchToY(topOfStaff, i, scale),
         leftOfNoteHead,
@@ -216,7 +228,7 @@ const drawLedgerLines = (params: DrawNoteParams) => {
   } else if (pitch >= 12) {
     // 12=A5
     for (let i = 12; i < pitch + 1; i += 2) {
-      drawLedgerLine(
+      section = drawLedgerLine(
         ctx,
         pitchToY(topOfStaff, i, scale),
         leftOfNoteHead,
@@ -225,19 +237,21 @@ const drawLedgerLines = (params: DrawNoteParams) => {
       );
     }
   }
+  return section;
 };
 
-const drawStemAndFlags = (params: DrawNoteParams) => {
+const drawStemAndFlags = (params: DrawNoteParams): DrawnSection | undefined => {
   const { ctx, topOfStaff, leftOfNoteHead, scale, note } = params;
   const { pitch, duration } = note;
   if (duration === 1) {
     return;
   }
-  const heightOfB3 = topOfStaff + (HEIGHT_STAFF_BRAVURA * scale) / 2;
-  const lineWidth = WIDTH_STEM * scale;
+  const heightOfB3 = topOfStaff + (bStaffHeight * scale) / 2;
+  const lineWidth = bStemWidth * scale;
   let stemCenter: number;
   let top: number;
   let bottom: number;
+  let drawnSection: DrawnSection | undefined;
   if (pitch < 6) {
     // B4未満 -> 上向き (楽譜の書き方p17)
     // 符頭の右に符幹がはみ出るのを補正 (lineWidth / 2)
@@ -252,17 +266,16 @@ const drawStemAndFlags = (params: DrawNoteParams) => {
       const index = duration <= 32 ? pitch + 7 : pitch + 8;
       top = pitchToY(topOfStaff, index, scale);
     }
-    const upFlag = upFlagMap.get(duration);
-    if (upFlag) {
-      const { path, stemUpNW } = upFlag;
-      ctx.save();
-      ctx.translate(
-        stemCenter - lineWidth / 2 + UNIT * stemUpNW.x * scale,
-        top + UNIT * stemUpNW.y * scale
+    const path = upFlagMap.get(duration);
+    if (path) {
+      drawBravuraPath(
+        ctx,
+        stemCenter - lineWidth / 2 + UNIT * path.stemUpNW.x * scale,
+        top + UNIT * path.stemUpNW.y * scale,
+        scale,
+        path
       );
-      ctx.scale(scale, -scale);
-      ctx.fill(path);
-      ctx.restore();
+      drawnSection = calcSection(leftOfNoteHead, scale, path);
     }
   } else {
     // 下向き
@@ -275,17 +288,16 @@ const drawStemAndFlags = (params: DrawNoteParams) => {
       const index = duration < 32 ? pitch - 7 : pitch - 8;
       bottom = pitchToY(topOfStaff, index, scale);
     }
-    const downFlag = downFlagMap.get(duration);
-    if (downFlag) {
-      const { path, stemDownSW } = downFlag;
-      ctx.save();
-      ctx.translate(
-        stemCenter - lineWidth / 2 + UNIT * stemDownSW.x * scale,
-        bottom + UNIT * stemDownSW.y * scale
+    const path = downFlagMap.get(duration);
+    if (path) {
+      drawBravuraPath(
+        ctx,
+        stemCenter - lineWidth / 2 + UNIT * path.stemDownSW.x * scale,
+        bottom + UNIT * path.stemDownSW.y * scale,
+        scale,
+        path
       );
-      ctx.scale(scale, -scale);
-      ctx.fill(path);
-      ctx.restore();
+      drawnSection = calcSection(leftOfNoteHead, scale, path);
     }
   }
 
@@ -298,25 +310,27 @@ const drawStemAndFlags = (params: DrawNoteParams) => {
   ctx.closePath();
   ctx.stroke();
   ctx.restore();
+
+  if (drawnSection) {
+    return drawnSection;
+  } else {
+    return { start: leftOfNoteHead, end: leftOfNoteHead + lineWidth };
+  }
 };
 
-const drawAccidental = (params: DrawNoteParams) => {
+const drawAccidental = (params: DrawNoteParams): DrawnSection | undefined => {
   const { ctx, leftOfNoteHead, topOfStaff, note, scale } = params;
   const { pitch, accidental } = note;
   if (!accidental) {
     return;
   }
   const top = pitchToY(topOfStaff, pitch, scale);
-  const { path, bbox } = accidentalPathMap.get(accidental)!;
-  const width = bbox.ne.x * UNIT;
+  const path = accidentalPathMap.get(accidental)!;
+  const width = path.bbox.ne.x * UNIT;
   const gap = UNIT / 3;
-  drawBravuraPath(
-    ctx,
-    leftOfNoteHead - (width + gap) * scale,
-    top,
-    scale,
-    path
-  );
+  const left = leftOfNoteHead - (width + gap) * scale;
+  drawBravuraPath(ctx, left, top, scale, path);
+  return calcSection(left, scale, path);
 };
 
 interface DrawNoteParams {
@@ -327,28 +341,61 @@ interface DrawNoteParams {
   scale: number;
 }
 
-const drawNote = (params: DrawNoteParams) => {
-  drawNoteHead(params);
-  drawLedgerLines(params);
-  drawStemAndFlags(params);
-  drawAccidental(params);
+/**
+ * 音符描画
+ * @param params: DrawNoteParams
+ * @returns 描画に使った幅
+ */
+const drawNote = (params: DrawNoteParams): number => {
+  const arr: (DrawnSection | undefined)[] = [
+    drawNoteHead(params),
+    drawLedgerLines(params),
+    drawStemAndFlags(params),
+    drawAccidental(params),
+  ];
+  let startX: number | undefined = undefined;
+  let endX: number | undefined = undefined;
+  arr.forEach((section) => {
+    if (!section) {
+      return;
+    }
+    const { start, end } = section;
+    if (startX === undefined || endX === undefined) {
+      startX = start;
+      endX = end;
+    } else {
+      if (startX > start) {
+        startX = start;
+      }
+      if (end > endX) {
+        endX = end;
+      }
+    }
+  });
+  return endX! - startX!;
 };
 
+/**
+ * 休符描画
+ * @returns 描画に使った幅
+ */
 const drawRest = (
   ctx: CanvasRenderingContext2D,
   topOfStaff: number,
   leftOfRest: number,
   rest: Rest,
   scale: number
-) => {
-  const { path, top } = restPathMap.get(rest.duration)!;
+): number => {
+  const path = restPathMap.get(rest.duration)!;
   drawBravuraPath(
     ctx,
     leftOfRest,
-    topOfStaff + UNIT * top * scale,
+    topOfStaff + UNIT * path.top * scale,
     scale,
     path
   );
+  const { start, end } = calcSection(leftOfRest, scale, path);
+  return end - start;
 };
 
 window.onload = () => {
@@ -361,7 +408,7 @@ window.onload = () => {
   const leftOfClef = marginHorizontal + 500 * scale;
   const elementGap = 1000 * scale;
   const elements: Element[] = [
-    { type: "note", pitch: 0, duration: 1 },
+    { type: "note", pitch: 10, duration: 1 },
     { type: "note", pitch: 7, duration: 4, accidental: "sharp" },
     { type: "note", pitch: -1, duration: 8, accidental: "flat" },
     { type: "note", pitch: 13, duration: 4 },
@@ -391,10 +438,30 @@ window.onload = () => {
   for (let i in elements) {
     const el = elements[i];
     const leftOfNoteHead = leftOfClef + elementGap * (parseInt(i) + 1);
+    ctx.save();
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(leftOfNoteHead, topOfStaff);
+    ctx.lineTo(leftOfNoteHead, bStaffHeight * scale);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+
+    let width;
     if (el.type === "note") {
-      drawNote({ ctx, topOfStaff, leftOfNoteHead, note: el, scale });
+      width = drawNote({ ctx, topOfStaff, leftOfNoteHead, note: el, scale });
     } else {
-      drawRest(ctx, topOfStaff, leftOfNoteHead, el, scale);
+      width = drawRest(ctx, topOfStaff, leftOfNoteHead, el, scale);
     }
+    ctx.save();
+    ctx.strokeStyle = "#FF0000";
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(leftOfNoteHead + width, topOfStaff);
+    ctx.lineTo(leftOfNoteHead + width, bStaffHeight * scale);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
   }
 };
